89df9bbc4fc0fc55706c781d81dd6a324fb2fa18
Miguel Ángel Bermea Rdz <73300671+MaikiBR@users.noreply.github.com>
2021-06-03 23:57:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 Team 4:
 José Ángel Tobón Salazar A01411655
 Miguel Ángel Bermea Rodríguez A01411671
-Jesús Eduardo Rodríguez Romero A01411
+Jesús Eduardo Rodríguez Romero A01411628
 Luis Ángel Ramiro Amaro A01411763
 Gustavo Luna Muñoz  A01411619
 */
